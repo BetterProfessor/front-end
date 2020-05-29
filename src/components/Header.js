d004cd128b7529877bf5b0a6e95122b9f7e958ad
Kristian Correa <kristiangcorrea@live.com>
2020-05-29 13:27:09 -0400

    Navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,21 +3,41 @@ import { Link } from 'react-router-dom'
 
 export default function header() {
   return (
-    <div className='back'>
-      <nav className='links'>
-        <h1>Better Professor</h1>
-        <div>
-          <Link className='navLinks' to={'/Register'}>
-            Register{' '}
-          </Link>
-          <Link className='navLinks' to={'/'}>
-            Login{' '}
-          </Link>
-          <Link className='navLinks' to={'Dashboard'}>
-            Dashboard
-          </Link>
+    <header>
+        <div style={{display: "flex"}}>
+          <img class="imglogo" src={require("../assets/NavIcon.png")} ></img>
+          <h2 class="logo">Better Professor</h2>
         </div>
-      </nav>
-    </div>
+    
+     <nav>
+         <a href="index.html">Home</a>
+         <a href="Team.html">Team</a>
+         <Link to={'Dashboard'}>
+           Dashboard
+          </Link>
+         <Link to={'/Register'}>
+          SignUp
+         </Link>
+         <Link to={'/'}>
+          Login
+         </Link>
+         
+     </nav>    
+    </header>
+    // <div className='back'>
+    //   <nav className='links'>
+    //     <div>
+    //       <Link className='navLinks' to={'/Register'}>
+    //         Register{' '}
+    //       </Link>
+    //       <Link className='navLinks' to={'/'}>
+    //         Login{' '}
+    //       </Link>
+    //       <Link className='navLinks' to={'Dashboard'}>
+    //         Dashboard
+    //       </Link>
+    //     </div>
+    //   </nav>
+    // </div>
   )
 }
